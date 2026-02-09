6bf2a1028226ce511e6472092c492b1f5389c544
Anri Dellal <anridellal@yandex.ru>
2026-02-09 22:14:12 +0300

    Fix #102

--- app/src/main/java/ru/henridellal/dialer/util/ContactsUtil.java
+++ app/src/main/java/ru/henridellal/dialer/util/ContactsUtil.java
@@ -36,7 +36,7 @@ public class ContactsUtil {
 	public static void view(Context context, Uri contentUri, String contactId) {
 		Intent intent = new Intent(Intent.ACTION_VIEW);
 		Uri uri = Uri.withAppendedPath(contentUri, contactId);
-		intent.setDataAndType(uri, "vnd.android.cursor.dir/contact");
+		intent.setDataAndType(uri, "vnd.android.cursor.item/contact");
 		try {
 			context.startActivity(intent);
 		} catch (ActivityNotFoundException e) {
